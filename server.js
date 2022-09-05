93289a7b8e24948670d8ee3a019ee2d2c4bcf427
Daniel Segura <danielseguraf@gmail.com>
2022-09-05 01:21:53 -0500

    express creating basic routes and responses

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
-import express from 'express'
+import express from 'express';
 import dotenv from 'dotenv';
 
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: './config/config.env' });
 console.log(process.env.PORT);
-
-const app = express()
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const app = express();
+
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
