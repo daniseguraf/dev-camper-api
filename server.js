98171c4fc17754b48dfb737806ba2b98aaa3f3ce
Daniel Segura <danielseguraf@gmail.com>
2022-09-07 17:37:26 -0500

    installing morgan

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bootcampsRoutes from './routes/bootcampsRoutes.js';
+import morgan from 'morgan';
 
 dotenv.config({ path: './config/config.env' });
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req, res) => res.send(`API is running on ${PORT}`));
 app.use('/api/v1/bootcamps', bootcampsRoutes);
